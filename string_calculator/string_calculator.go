95d7aae8e52cf182cefa16d0d9a0bbc4ff2cdb8a
Aleksey Vasyukov <avvasyukov@avito.ru>
2021-06-11 20:21:08 +0300

    rename helper funcs

--- string_calculator/string_calculator.go
+++ string_calculator/string_calculator.go
@@ -8,58 +8,12 @@ import (
 	"unicode/utf8"
 )
 
-func NewDelimiters(line string) ([]string, error) {
-	var delimiters []string
-
-	line = strings.TrimPrefix(line, "//")
-	if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
-		re := regexp.MustCompile(`\[(.*?)\]`)
-		delimiters = re.FindAllString(line, -1)
-		for i := range delimiters {
-			delimiters[i] = strings.TrimPrefix(delimiters[i], "[")
-			delimiters[i] = strings.TrimSuffix(delimiters[i], "]")
-		}
-	} else if utf8.RuneCountInString(line) == 1 {
-		delimiters = append(delimiters, line)
-	} else {
-		return nil, fmt.Errorf("wrong delimiter")
-	}
-
-	return delimiters, nil
-}
-
-func GetDelimiters(input string) (string, []string, error) {
-	var delimiters []string
-
-	// Change delimiter: `//[delimiter]\n[numbers...]`
-	if strings.HasPrefix(input, "//") {
-		lines := strings.SplitN(input, "\n", 2)
-		if len(lines) != 2 {
-			return "", nil, fmt.Errorf("wrong delimiter")
-		}
-
-		var err error
-		delimiters, err = NewDelimiters(lines[0])
-		if err != nil {
-			return "", nil, err
-		}
-
-		input = lines[1]
-	} else {
-		delimiters = append(delimiters, ",")
-	}
-
-	delimiters = append(delimiters, "\n")
-
-	return input, delimiters, nil
-}
-
 func Add(input string) (int, error) {
 	if input == "" {
 		return 0, nil
 	}
 
-	numbersString, delimiters, err := GetDelimiters(input)
+	numbersString, delimiters, err := getDelimiters(input)
 	if err != nil {
 		return 0, err
 	}
@@ -97,4 +51,50 @@ func Add(input string) (int, error) {
 	}
 
 	return sum, nil
+}
+
+func getDelimiters(input string) (string, []string, error) {
+	var delimiters []string
+
+	// Change delimiter: `//[delimiter]\n[numbers...]`
+	if strings.HasPrefix(input, "//") {
+		lines := strings.SplitN(input, "\n", 2)
+		if len(lines) != 2 {
+			return "", nil, fmt.Errorf("wrong delimiter")
+		}
+
+		var err error
+		delimiters, err = getNewDelimiters(lines[0])
+		if err != nil {
+			return "", nil, err
+		}
+
+		input = lines[1]
+	} else {
+		delimiters = append(delimiters, ",")
+	}
+
+	delimiters = append(delimiters, "\n")
+
+	return input, delimiters, nil
+}
+
+func getNewDelimiters(line string) ([]string, error) {
+	var delimiters []string
+
+	line = strings.TrimPrefix(line, "//")
+	if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
+		re := regexp.MustCompile(`\[(.*?)\]`)
+		delimiters = re.FindAllString(line, -1)
+		for i := range delimiters {
+			delimiters[i] = strings.TrimPrefix(delimiters[i], "[")
+			delimiters[i] = strings.TrimSuffix(delimiters[i], "]")
+		}
+	} else if utf8.RuneCountInString(line) == 1 {
+		delimiters = append(delimiters, line)
+	} else {
+		return nil, fmt.Errorf("wrong delimiter")
+	}
+
+	return delimiters, nil
 }
